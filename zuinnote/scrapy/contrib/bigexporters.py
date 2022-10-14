2d3ded1f02a0c5191b04d0b65d88763e6b63f858
Kevin Grismore <kevin.grismore@illumis.com>
2022-10-14 13:23:06 -0400

    use pyarrow instead

--- zuinnote/scrapy/contrib/bigexporters.py
+++ zuinnote/scrapy/contrib/bigexporters.py
@@ -75,18 +75,17 @@ see: https://docs.scrapy.org/en/latest/topics/exporters.html
 from scrapy.exporters import BaseItemExporter
 from scrapy.utils.project import get_project_settings
 
-
 import logging
 
 SUPPORTED_EXPORTERS = {}
 ### Check which libraries are available for the exporters
 try:
-    #from fastparquet import write as fp_write
-    #import pandas as pd
+    import pyarrow as pa
+    from pyarrow import fs
 
     SUPPORTED_EXPORTERS["parquet"] = True
     logging.getLogger().info(
-        "Successfully imported fastparquet. Export to parquet supported."
+        "Successfully imported pyarrow. Export to parquet supported."
     )
 except ImportError:
     SUPPORTED_EXPORTERS["parquet"] = False
@@ -103,43 +102,69 @@ class ParquetItemExporter(BaseItemExporter):
         """
         super().__init__(**kwargs)
         self.file = file  # file name
-        self.itemcount = 0  # initial item count
-        self.columns = []  # initial columns to export
+        self.item_count = 0
+        self.file_count = 0
+        self.records = []
         self.logger = logging.getLogger()
         self._configure(kwargs, dont_fail=dont_fail)
+        self.filesystem = self._create_filesystem()
 
     def _configure(self, options, dont_fail=False):
         """Configure the exporter by poping options from the ``options`` dict.
         If dont_fail is set, it won't raise an exception on unexpected options
         (useful for using with keyword arguments in subclasses ``__init__`` methods)
         """
+        # Inherited options
         self.encoding = options.pop("encoding", None)
         self.fields_to_export = options.pop("fields_to_export", None)
         self.export_empty_fields = options.pop("export_empty_fields", False)
-        # Read settings
-        self.pq_compression = options.pop("compression", "GZIP")
-        self.pq_times = options.pop("times", "int64")
-        self.pq_objectencoding = options.pop("objectencoding", "infer")
-        self.pq_convertstr = options.pop("convertallstrings", False)
-        self.pq_hasnulls = options.pop("hasnulls", True)
-        self.pq_writeindex = options.pop("writeindex", False)
-        self.pq_items_rowgroup = options.pop("items_rowgroup", 10000)
-        self.pq_rowgroupoffset = options.pop("rowgroupoffset", 50000000)
+        # Options passed through feed settings
+        # Params for write_table
+        self.compression = options.pop("compression", "SNAPPY")
+        # Other control options
+        self.items_per_file = options.pop("items_per_file", 10000)
+        self.gcp_service_account_info = options.pop("gcp_service_account_info", None)
+
+    def _create_filesystem(self):
+        """
+        Check for a prefix on the file name string and create the corresponding
+        pyarrow filesystem
+        """
+
+        if self.file.startswith("gcs://"):
+            return fs.GcsFileSystem(target_service_account=self.gcp_service_account_info)
+
+        # elif
+
+        else:
+            return fs.LocalFileSystem()
 
     def export_item(self, item):
         """
-        Export a specific item to the file
+        Add a record to the PyarrowBatchBuffer. Write a file if items_per_file is exceeded.
         """
-        # Initialize writer
-        if len(self.columns) == 0:
-            self._init_table(item)
-        # Create a new row group to write
-        if self.itemcount > self.pq_items_rowgroup:
-            self._flush_table()
-        # Add the item to data frame
-        self.df = pd.concat([self.df, self._get_df_from_item(item)])
-        self.itemcount += 1
-        return item
+        if self.item_count > self.items_per_file:
+            self._write_table_to_file()
+
+        record = dict(self._get_serialized_fields())
+        self.records.append(record)
+
+        self.item_count += 1
+
+    def _write_table_to_file(self):
+        """
+        Turn the PyarrowBatchBuffer into a Table, then write it to a file.
+        """
+        table = pa.Table.from_pylist(self.records)
+        pa.parquet.write_table(
+            table=table,
+            where=f"{self.file}-{self.file_count}",
+            compression=self.compression,
+            filesystem=self.filesystem,
+        )
+        
+        self.item_count = 0
+        self.file_count += 1
 
     def start_exporting(self):
         """
@@ -149,88 +174,9 @@ class ParquetItemExporter(BaseItemExporter):
             raise RuntimeError(
                 "Error: Cannot export to parquet. Cannot import fastparquet. Have you installed it?"
             )
-        self.firstBlock = True  # first block of parquet file
 
     def finish_exporting(self):
         """
         Triggered when Scrapy ends exporting. Useful to shutdown threads, close files etc.
         """
-        self._flush_table()
-
-    def _get_columns(self, item):
-        """
-        Determines the columns of an item
-        """
-        if isinstance(item, dict):
-            # for dicts try using fields of the first item
-            self.columns = list(item.keys())
-        elif hasattr(item, "__dataclass_fields__"):
-             # for dataclasses
-            self.columns = list(getattr(item, "__dataclass_fields__").keys())
-        else:
-            # use fields declared in Item
-            self.columns = list(item.fields.keys())
-
-    def _init_table(self, item):
-        """
-        Initializes table for parquet file
-        """
-        # initialize columns
-        self._get_columns(item)
-        self._reset_rowgroup()
-
-    def _get_df_from_item(self, item):
-        """
-        Get the dataframe from item
-        """
-        row = {}
-        fields = dict(
-            self._get_serialized_fields(item, default_value="", include_empty=True)
-        )
-        for column in self.columns:
-            if self.pq_convertstr == True:
-                row[column] = str(fields.get(column, None))
-            else:
-                value = fields.get(column, None)
-                row[column] = value
-        if self.pq_convertstr == True:
-            return pd.DataFrame(row, index=[0]).astype(str)
-        return pd.DataFrame.from_dict([row])
-
-    def _reset_rowgroup(self):
-        """
-        Reset dataframe for writing
-        """
-        if self.pq_convertstr == False:  # auto determine schema
-            # initialize df
-            self.df = pd.DataFrame(columns=self.columns)
-        else:
-            # initialize df with zero strings to derive correct schema
-            self.df = pd.DataFrame(columns=self.columns).astype(str)
-
-    def _flush_table(self):
-        """
-        Writes the current row group to parquet file
-        """
-        if len(self.df.index) > 0:
-            # reset written entries
-            self.itemcount = 0
-            # write existing dataframe as rowgroup to parquet file
-            papp = True
-            if self.firstBlock == True:
-                self.firstBlock = False
-                papp = False
-            fp_write(
-                self.file.name,
-                self.df,
-                append=papp,
-                compression=self.pq_compression,
-                has_nulls=self.pq_hasnulls,
-                write_index=self.pq_writeindex,
-                file_scheme="simple",
-                object_encoding=self.pq_objectencoding,
-                times=self.pq_times,
-                row_group_offsets=self.pq_rowgroupoffset,
-            )
-            # initialize new data frame for new row group
-            self._reset_rowgroup()
+        self._write_table_to_file()
